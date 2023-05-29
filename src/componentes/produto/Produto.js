5b1e20c4ab263b56c53847ec52444cbaa1c28310
JONATHAN DA SILVA PEREIRA <jhoemen@gmail.com>
2023-05-29 00:03:43 -0400

    Inclusão placeholder card produto

--- src/componentes/produto/Produto.js
+++ src/componentes/produto/Produto.js
@@ -1,17 +1,35 @@
-import { CFormLabel, CFormInput, CFormText, CCard, CCardImage, CCardBody, CCardTitle, CCardText, CButton, CCardSubtitle } from '@coreui/react'
+import { CFormLabel, CFormInput, CFormText, CCard, CCardImage, CCardBody, CCardTitle, CCardText, CButton, CCardSubtitle, CPlaceholder } from '@coreui/react'
 
 export default function ProdutoComponente(props) {
     const { item: produto = [] } = props
 
     return (
         <>
-            <CCard>
+            <CCard className="mb-4">
                 <CCardImage orientation="top" src={produto.imagem} />
                 <CCardBody>
                     <CCardTitle>{produto.nome}</CCardTitle>
                     <CCardSubtitle className="mb-2 text-medium-emphasis">{produto.preco}</CCardSubtitle>
                     <CCardText>{produto.descricao}</CCardText>
-                    <CButton href="#">Adicionar ao Carrinho</CButton>
+                    <CButton className="text-end">Adicionar ao Carrinho</CButton>
+                </CCardBody>
+            </CCard>
+            <CCard>
+                <CCardImage component="svg" orientation="top" width="100%" height="162" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false">
+                    <title>{produto.nome}</title>
+                    <rect width="100%" height="100%" fill="#868e96"></rect>
+                </CCardImage>
+                <CCardBody>
+                    <CPlaceholder component={CCardTitle} animation="glow" xs={7}>
+                        <CPlaceholder xs={6} />
+                    </CPlaceholder>
+                    <CPlaceholder component={CCardText} animation="glow">
+                        <CPlaceholder xs={7} />
+                        <CPlaceholder xs={4} />
+                        <CPlaceholder xs={4} />
+                        <CPlaceholder xs={6} />
+                    </CPlaceholder>
+                    <CPlaceholder component={CButton} disabled href="#" tabIndex={-1} xs={6}></CPlaceholder>
                 </CCardBody>
             </CCard>
         </>
