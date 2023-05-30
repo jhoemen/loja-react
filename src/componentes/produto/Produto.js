d5cdf2cf5e8a2bd2034363a9f9f47d0c3fc860ce
JONATHAN DA SILVA PEREIRA <jhoemen@gmail.com>
2023-05-29 23:26:45 -0400

    Inclusão select carregamento card produtos

--- src/componentes/produto/Produto.js
+++ src/componentes/produto/Produto.js
@@ -1,42 +1,48 @@
-import { CFormLabel, CFormInput, CFormText, CCard, CCardImage, CCardBody, CCardTitle, CCardText, CButton, CCardSubtitle, CPlaceholder } from '@coreui/react'
+import { CFormLabel, CFormInput, CFormText, CCard, CCardImage, CCardBody, CCardTitle, CCardText, CButton, CCardSubtitle, CPlaceholder, CSpinner } from '@coreui/react'
+import { Suspense } from 'react'
+
+const loading = (
+    <CCard>
+        <CCardImage component="svg" orientation="top" width="100%" height="162" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false">
+            <title>xxxxxxxx</title>
+            <rect width="100%" height="100%" fill="#868e96"></rect>
+        </CCardImage>
+        <CCardBody>
+            <CPlaceholder component={CCardTitle} animation="glow" xs={7}>
+                <CPlaceholder xs={6} />
+            </CPlaceholder>
+            <CPlaceholder component={CCardText} animation="glow">
+                <CPlaceholder xs={7} />
+                <CPlaceholder xs={4} />
+                <CPlaceholder xs={4} />
+                <CPlaceholder xs={6} />
+            </CPlaceholder>
+            <CPlaceholder component={CButton} disabled href="#" tabIndex={-1} xs={6}></CPlaceholder>
+        </CCardBody>
+    </CCard>
+)
 
 export default function Produto(props) {
     const { item: produto = [], adicionarProdutoCarrinho, removerProdutoCarrinho } = props
 
     return (
         <>
-            <CCard className="mb-4">
-                <CCardImage orientation="top" src={produto.imagem} />
-                <CCardBody>
-                    <CCardTitle>{produto.nome}</CCardTitle>
-                    <CCardSubtitle className="mb-2 text-medium-emphasis">{produto.preco}</CCardSubtitle>
-                    <CCardText>{produto.descricao}</CCardText>
-                    <CButton
-                        onClick={(e) => {
-                            adicionarProdutoCarrinho({ ...produto, quantidade: 1 })
-                        }}>
-                        Adicionar ao Carrinho
-                    </CButton>
-                </CCardBody>
-            </CCard>
-            <CCard>
-                <CCardImage component="svg" orientation="top" width="100%" height="162" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false">
-                    <title>{produto.nome}</title>
-                    <rect width="100%" height="100%" fill="#868e96"></rect>
-                </CCardImage>
-                <CCardBody>
-                    <CPlaceholder component={CCardTitle} animation="glow" xs={7}>
-                        <CPlaceholder xs={6} />
-                    </CPlaceholder>
-                    <CPlaceholder component={CCardText} animation="glow">
-                        <CPlaceholder xs={7} />
-                        <CPlaceholder xs={4} />
-                        <CPlaceholder xs={4} />
-                        <CPlaceholder xs={6} />
-                    </CPlaceholder>
-                    <CPlaceholder component={CButton} disabled href="#" tabIndex={-1} xs={6}></CPlaceholder>
-                </CCardBody>
-            </CCard>
+            <Suspense fallback={loading}>
+                <CCard className="mb-4">
+                    <CCardImage orientation="top" src={produto.imagem} />
+                    <CCardBody>
+                        <CCardTitle>{produto.nome}</CCardTitle>
+                        <CCardSubtitle className="mb-2 text-medium-emphasis">{produto.preco}</CCardSubtitle>
+                        <CCardText>{produto.descricao}</CCardText>
+                        <CButton
+                            onClick={(e) => {
+                                adicionarProdutoCarrinho({ ...produto, quantidade: 1 })
+                            }}>
+                            Adicionar ao Carrinho
+                        </CButton>
+                    </CCardBody>
+                </CCard>
+            </Suspense>
         </>
     )
 }
