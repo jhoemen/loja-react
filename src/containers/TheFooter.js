1a65dfeffcd6d7c8afae23d0a6a1eaed655b6168
JONATHAN DA SILVA PEREIRA <jhoemen@gmail.com>
2023-05-31 23:48:53 -0400

    Refatoração rodapé

--- src/containers/TheFooter.js
+++ src/containers/TheFooter.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { CFooter } from '@coreui/react'
+import { CFooter, CLink } from '@coreui/react'
 
 const TheFooter = (props) => {
     const { totalCarrinho } = props
@@ -8,9 +8,20 @@ const TheFooter = (props) => {
     var year = today.getFullYear()
 
     return (
-        <CFooter>
-            <div className="width-100 text-center">
-                <span>Copyright &copy; {year} - Loja Online</span>
+        <CFooter className="bg-white text-dark">
+            <div>
+                <CLink className="text-decoration-none text-dark" href="https://coreui.io">
+                    Loja Online
+                </CLink>
+                <span>
+                    <b> - &copy; 2023</b>
+                </span>
+            </div>
+            <div>
+                <span>Desenvolvido por </span>
+                <CLink className="text-decoration-none text-dark" href="https://github.com/jhoemen">
+                    <b>Jonathan da Silva Pereira</b>
+                </CLink>
             </div>
         </CFooter>
     )
